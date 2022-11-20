a2611036263c6ccea6e82e359c76c7f06e40576c
Asriellol <41880257+Asriellol@users.noreply.github.com>
2022-11-19 19:48:39 -0500

    Added new feature

--- client.py
+++ client.py
@@ -1,12 +1,14 @@
 import threading
 import sys
 import socket 
+import os
 
 ## Client.py
 s = socket.socket()
 host = input(str("Please enter the hostname of the server: "))
 port = 9999
 s.connect((host, port))
+os.system("title Chat room connected at " + host)
 print("Connected to chat server.")
 
 ## Open a socket, connect to the server of the users choice at port 9999.
@@ -42,6 +44,17 @@ threading.Thread(target=receive).start()
 ## When the user connects to the server, take their first message and set it as their name.
 name = conn.recv(1024)
 name = name.decode('ascii')
+
+
+
+## Check each message from the server, and collect the text stored in the beginning surrounded by <> and put it into an array called "Users"
+while True:
+    message = conn.recv(1024)
+    message = message.decode('ascii')
+    if "<" in message:
+        message = message.split("<")
+        message = message[1].split(">")
+        Users.append(message[0])
+
+
 print("Connected with " + name)
-
-
